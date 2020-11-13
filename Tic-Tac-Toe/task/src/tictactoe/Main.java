cd5d9223891f3939bb21821240ce9c5f3d70ef82
denamyte <denamyte@gmail.com>
2020-11-13 20:49:09 +0200

    2020-11-13: solving Stage 5/5 2

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -6,8 +6,12 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    public static final int X_SUM = 264;
-    public static final int O_SUM = 237;
+    public static final int X_SUM = 264;  // 'X' + 'X' + 'X'
+    public static final int O_SUM = 237;  // 'O' + 'O' + 'O'
+    public static final String ENTER = "Enter the coordinates: ";
+    public static final String ERROR_OCCUPIED = "This cell is occupied! Choose another one!\n";
+    public static final String ERROR_NOT_NUMBERS = "You should enter numbers!\n";
+    public static final String ERROR_NOT_1_3 = "Coordinates should be from 1 to 3!\n";
     // Derived
     private final Map<Integer, Integer> sumsMap = new HashMap<>() {{
         put(X_SUM, 0);
@@ -21,87 +25,44 @@ public class Main {
     // Obtained
     private char[] data;
     private int movesCount;
-    private int errorString;
+    private boolean xsTurn = true;
+    private String additionalString;  // one of three errors | draw message | win message
+    private int[] crd;
     @Deprecated
     private GameState state = GameState.START;
     private GameState2 state2 = GameState2.ENTER_CELLS;
 
     public Main() {
         data = new char[9];
-        Arrays.fill(data, '_');
+        Arrays.fill(data, ' ');
     }
 
-    public void manage(String input) {
-        render(true);
-
-        switch (state) {
-            case START:
-                turnOnState(GameState.ENTER_CELLS, "Enter cells: ");
-                break;
-            case ENTER_CELLS:
-                enterCells(input);
-                turnOnEnterCrd();
-                break;
-            case ENTER_CRD:
-                manageEnteringCoordinates(input);
-                break;
-        }
+    public boolean notFinished() {
+        System.out.print(render());
+        return state2 != GameState2.FINISHED;
     }
 
-    @Deprecated
-    private void turnOnEnterCrd() {
-        turnOnState(GameState.ENTER_CRD, "Enter the coordinates: ");
+    public void input(String input) {
+        parseAndCheckCrd(input);
+
+        // TODO: 11/13/20 Define state here
+
     }
 
-    private void turnOnState(GameState state, String msg) {
-        this.state = state;
-        if (msg != null) {
-            System.out.print(msg);
-        }
-    }
-
-    private void enterCells(String input) {
-        data = input.toCharArray();
-        System.out.println(render(false));
-    }
-
-    private void manageEnteringCoordinates(String input) {
-        int[] crd = parseAndCheckCrd(input);
-        if (crd != null) {
-            setFieldByCrd('X', crd);
-            turnOnState(GameState.DONE, render(false) + '\n');
-        } else {
-            turnOnEnterCrd();
-        }
-    }
-
-    private int[] parseAndCheckCrd(String input) {
+    private void parseAndCheckCrd(String input) {
+        state2 = GameState2.ERROR;
         if (!input.matches("\\d +\\d")) {
-            System.out.println("You should enter numbers!");
-            return null;
+            additionalString = ERROR_NOT_NUMBERS;
+        } else if (!input.matches("[1-3] +[1-3]")) {
+            additionalString = ERROR_NOT_1_3;
+        } else {
+            crd = Arrays.stream(input.split(" +")).mapToInt(Integer::parseInt).toArray();
+            if (getFieldByCrd(crd) != ' ') {
+                additionalString = ERROR_OCCUPIED;
+            } else {
+                state2 = GameState2.ENTER_CELLS;
+            }
         }
-        // Here the input is 2 numbers in range [0, 9]
-
-        if (!input.matches("[1-3] +[1-3]")) {
-            System.out.println("Coordinates should be from 1 to 3!");
-            return null;
-        }
-        // Here, the input is 2 numbers in range [1, 3]
-
-        int[] crd = Arrays.stream(input.split(" +")).mapToInt(Integer::parseInt).toArray();
-        if (isFieldByCrdOccupied(crd)) {
-            return null;
-        }
-        return crd;
-    }
-
-    private boolean isFieldByCrdOccupied(int[] crd) {
-        char field = getFieldByCrd(crd);
-        if (field == 'X' || field == 'O') {
-            System.out.println("This cell is occupied! Choose another one!");
-            return true;
-        }
-        return false;
     }
 
     private char getFieldByCrd(int[] crd) {
@@ -116,26 +77,32 @@ public class Main {
         return 8 - 3 * crd[1] + crd[0];
     }
 
-    public String render(boolean includeGameState) {
-        // TODO: 11/13/20 render should include errors
+    public String render() {
         StringBuilder sb = new StringBuilder();
+        if (state2 != GameState2.ERROR) {
+            renderTable(sb);
+        }
+        if (state2 != GameState2.ENTER_CELLS){
+            sb.append(additionalString);
+        }
+        if (state2 != GameState2.FINISHED) {
+            sb.append(ENTER);
+        }
+        return sb.toString();
+    }
+
+    private void renderTable(StringBuilder sb) {
         sb.append("---------\n");
         for (int row = 0; row < 3; row++) {
             bodyLine(sb, row * 3);
         }
         sb.append("---------");
-
-        if (includeGameState) {
-            sb.append(chooseGameState()).append('\n');
-        }
-        return sb.toString();
     }
 
     private void bodyLine(StringBuilder sb, int shift) {
         sb.append("| ");
         for (int x = 0; x < 3; x++) {
-            char field = data[shift + x];
-            sb.append(field == '_' ? ' ' : field).append(' ');
+            sb.append(data[shift + x]).append(' ');
         }
         sb.append("|\n");
     }
@@ -189,14 +156,15 @@ public class Main {
 
     enum GameState2 {
         ENTER_CELLS,
-        ERROR
+        ERROR,
+        FINISHED
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Main ticTac = new Main();
-        while (ticTac.state != GameState.DONE) {
-            ticTac.manage(scanner.nextLine());
+        while (ticTac.notFinished()) {
+            ticTac.input(scanner.nextLine());
         }
     }
 }
